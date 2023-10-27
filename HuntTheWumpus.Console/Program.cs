b539ad81fd012302f7ee21119debd78221fcd9b9
scrawach <scrawach@gmail.com>
2023-10-27 12:16:45 +0300

    implemented player input

--- HuntTheWumpus.Console/Program.cs
+++ HuntTheWumpus.Console/Program.cs
@@ -1,6 +1,5 @@
 ﻿using HuntTheWumpus.Console;
 using HuntTheWumpus.Core;
-using HuntTheWumpus.Core.AI;
 using HuntTheWumpus.Core.Common;
 
 var settings = new GameSettings()
@@ -13,15 +12,15 @@ game.Initialize(settings);
 
 var worldTextView = new WorldTextView(game.Mechanics.World, game.Mechanics.Actors);
 var gameEndScreen = new GameEndScreen(game);
-var playerTurnMaker = new EmptyIntellect();
+var playerTurnMaker = new PlayerInput(game.Mechanics.World);
 
+Console.WriteLine(worldTextView);
 while (game.Result.IsProcess)
 {
     foreach (var command in game.Update(playerTurnMaker)) 
         Console.WriteLine($"Executed {command}");
 
-    Console.WriteLine(worldTextView);
-    await Task.Delay(200);
     Console.Clear();
+    Console.WriteLine(worldTextView);
 }
 Console.WriteLine(gameEndScreen);
